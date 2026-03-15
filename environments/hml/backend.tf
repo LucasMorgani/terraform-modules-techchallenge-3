1a4809707b39e3536d7bccfbd4c780e28712b9c6
LucasMorgani <morganis1411@gmail.com>
2026-03-15 19:31:19 -0300

    adding workflows and environment

--- environments/hml/backend.tf
+++ environments/hml/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-state-techchallenge-equipe7"
-    key    = "techchallenge3/terraform.tfstate"
+    key    = "techchallenge3/hml/terraform.tfstate"
     region = "us-east-1"
   }
 }
