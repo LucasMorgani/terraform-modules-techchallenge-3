1f68123886c9ce3c6d588e384bdc06f3b57f836f
LucasMorgani <morganis1411@gmail.com>
2026-03-16 14:33:50 -0300

    fix: changing variables

--- modules/databases/outputs.tf
+++ modules/databases/outputs.tf
@@ -10,6 +10,6 @@ output "rds_instance_id" {
 
 output "dynamodb_table_name" {
   description = "DynamoDB table name"
-  value = module.dynamodb_table.dynamodb_table_name
+  value = module.dynamodb_table.dynamodb_table_id
 }
 
